fa4c90bf53e36beabf2a0e7c41b6f58b45e002f1
Estevão <101600957+estevaopolli@users.noreply.github.com>
2022-05-04 13:38:17 -0300

    Day 2

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ function onScroll(){
         navigation.classList.remove("scroll");
     }
 }
+
+function openMenu(){
+    document.body.classList.add("menuExpanded")
+}
+
+function closeMenu(){
+    document.body.classList.remove("menuExpanded")
+}
