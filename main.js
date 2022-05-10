c7bf125a1f61b7c69c02c3d980e8b2c9d44b24db
Estevão <101600957+estevaopolli@users.noreply.github.com>
2022-05-10 12:20:37 -0300

    Final day

--- main.js
+++ main.js
@@ -4,6 +4,36 @@ onScroll();
 function onScroll(){
     showNavOnScroll();
     showBackToTopButtonOnScroll();
+    ActivateMenuAtCurrentSection(home);
+    ActivateMenuAtCurrentSection(services);
+    ActivateMenuAtCurrentSection(about);
+}
+
+function ActivateMenuAtCurrentSection(section){
+    const targetLine = scrollY + innerHeight / 2;
+
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+
+    const sectionTopReachOrPassedTargetline = targetLine >= sectionTop;
+    console.log('O topo da seção chegou ou passou da linha?', sectionTopReachOrPassedTargetline);
+
+    const sectionEndsAt = sectionTop + sectionHeight;
+
+    const sectionEndPassedTargetline = sectionEndsAt <= targetLine;
+    console.log('A base da seção passou da linha?', sectionEndPassedTargetline);
+
+    //limites da seção
+    const sectionBoudaries = sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline;
+    console.log(sectionBoudaries);
+
+    const sectionId = section.getAttribute('id');
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active')
+    if (sectionBoudaries){
+        menuElement.classList.add('active')
+    }
 }
 
 function showNavOnScroll(){
